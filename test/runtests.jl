c24a97b473757edd72e5681ca5982d36ebf41513
aknudson <alexk.706@gmail.com>
2023-05-31 08:43:49 -0700

    Added tests for some utility functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,76 @@
 using Test
 using PearsonCorrelationMatch
+using PearsonCorrelationMatch: _generate_coefs, _hermite, _hermite_normpdf, _feasible_roots, _nearest_root
+using Polynomials
+
+
+@testset "Utilities" begin
+    @testset "Generate Coefficients" begin
+        dA = Binomial(20, 0.2)
+        dB = NegativeBinomial(20, 0.002)
+        dC = LogitNormal(3, 1)
+        dD = Beta(5, 3)
+
+        @test_nowarn _generate_coefs(dA, 7)
+        @test_nowarn _generate_coefs(dB, 7)
+        @test_nowarn _generate_coefs(dC, 7)
+        @test_nowarn _generate_coefs(dD, 7)
+
+        @test_nowarn _generate_coefs(dA, 7.0)
+        @test_nowarn _generate_coefs(dB, 7.0)
+        @test_nowarn _generate_coefs(dC, 7.0)
+        @test_nowarn _generate_coefs(dD, 7.0)
+
+        @test_throws InexactError _generate_coefs(dA, 7.5)
+        @test_throws InexactError _generate_coefs(dB, 7.5)
+        @test_throws InexactError _generate_coefs(dC, 7.5)
+        @test_throws InexactError _generate_coefs(dD, 7.5)
+    end
+
+    @testset "Hermite Evaluation" begin
+        
+    end
+
+    @testset "Solve Polynomial on [-1, 1]" begin
+        r1 = nextfloat(-1.0)
+        r2 = prevfloat(1.0)
+        r3 = eps()
+        r4 = prevfloat(2.0) * rand() - prevfloat(1.0)
+
+        P1 = coeffs(3 * fromroots([r1, 7, 7, 8]))
+        P2 = coeffs(-5 * fromroots([r2, -1.14, -1.14, -1.14, -1.14, 1119]))
+        P3 = coeffs(1.2 * fromroots([r3, nextfloat(1.0), prevfloat(-1.0)]))
+        P4 = coeffs(fromroots([-5, 5, r4]))
+        P5 = coeffs(fromroots([nextfloat(1.0), prevfloat(-1.0)]))
+        P6 = coeffs(fromroots([-0.5, 0.5]))
+
+        # One root at -1.0
+        xs = _feasible_roots(P1)
+        @test only(xs) ≈ r1 atol=0.001
+
+        # One root at 1.0
+        xs = _feasible_roots(P2)
+        @test only(xs) ≈ r2 atol=0.001
+
+        # Roots that are just outside [-1, 1]
+        xs = _feasible_roots(P3)
+        @test only(xs) ≈ r3 atol=0.001
+
+        # Random root in (-1, 1)
+        xs = _feasible_roots(P4)
+        @test only(xs) ≈ r4 atol=0.001
+
+        # Case of no roots
+        xs = _feasible_roots(P5)
+        @test length(xs) == 0
+        
+        # Case of multiple roots
+        xs = _feasible_roots(P6)
+        @test length(xs) == 2
+        @test _nearest_root(-0.6, xs) ≈ -0.5 atol=0.001
+    end
+end
+
 
 @testset "Pearson Correlation Matching" begin
     dA = Beta(2, 3)

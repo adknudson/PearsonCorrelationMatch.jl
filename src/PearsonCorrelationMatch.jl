140b67531993814fd08bb2447fc80e0ae34c118a
aknudson <alexk.706@gmail.com>
2024-03-17 23:20:06 -0700

    put compile workload into let block to avoid exporting temp vars

--- src/PearsonCorrelationMatch.jl
+++ src/PearsonCorrelationMatch.jl
@@ -7,7 +7,7 @@ using Distributions: mean, std, quantile, cdf
 
 using FastGaussQuadrature: gausshermite
 using IrrationalConstants: sqrt2, invsqrtπ
-using LinearAlgebra: eigen, Symmetric, Diagonal
+using LinearAlgebra: eigen, Symmetric, Diagonal, lmul!, rmul!, diagind
 using PolynomialRoots: roots
 using SharedArrays: SharedMatrix, sdata
 using StatsFuns: normcdf, normpdf, norminvcdf
@@ -25,25 +25,29 @@ using PrecompileTools
 using Distributions: Distributions
 
 @setup_workload begin
-    p = 0.5
-    D = Distributions.Gamma()
-    E = Distributions.Beta(5, 3)
-    F = Distributions.Binomial(100, 0.3)
-    G = Distributions.NegativeBinomial(20)
-    margins = [D, E, F, G]
+    # put in let-block to avoid exporting temporary variables
+    # TODO: check if wrapping in let-block is an anti-pattern
+    let
+        p = 0.5
+        D = Distributions.Gamma()
+        E = Distributions.Beta(5, 3)
+        F = Distributions.Binomial(100, 0.3)
+        G = Distributions.NegativeBinomial(20)
+        margins = [D, E, F, G]
 
-    @compile_workload begin
-        pearson_bounds(D, E)
-        pearson_bounds(D, F)
-        pearson_bounds(G, D)
-        pearson_bounds(F, G)
-        pearson_bounds(margins)
+        @compile_workload begin
+            pearson_bounds(D, E)
+            pearson_bounds(D, F)
+            pearson_bounds(G, D)
+            pearson_bounds(F, G)
+            pearson_bounds(margins)
 
-        pearson_match(p, D, E)
-        pearson_match(p, D, F)
-        pearson_match(p, G, D)
-        pearson_match(p, F, G)
-        pearson_match([1.0 0.5; 0.5 1.0], [E, G])
+            pearson_match(p, D, E)
+            pearson_match(p, D, F)
+            pearson_match(p, G, D)
+            pearson_match(p, F, G)
+            pearson_match([1.0 0.5; 0.5 1.0], [E, G])
+        end
     end
 end
 

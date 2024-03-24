d56b19d2a07ac1fcbafd37a947bece7f5aedcdcb
aknudson <alexk.706@gmail.com>
2024-03-23 19:43:40 -0700

    updated rules to use kwargs

--- src/rules.jl
+++ src/rules.jl
@@ -1,5 +1,5 @@
 """
-    _rule(p, d1, d2, n)
+    _rule(p, d1, d2; kwargs...)
 
 Finds ``G(p) = pₓ`` where ``p`` is the correlation used in a bivariate Gaussian copula, and
 ``pₓ`` is the resulting correlation between the two marginals after the NORTA step.
@@ -7,7 +7,7 @@ Finds ``G(p) = pₓ`` where ``p`` is the correlation used in a bivariate Gaussia
 function _rule end
 
 """
-_invrule(p, d1, d2, n)
+_invrule(p, d1, d2; kwargs...)
 
 Finds ``Ginv(pₓ) = p`` where ``pₓ`` is the target correlation between two marginals, and
 ``p`` is the correlation to be used in a bivariate Gaussian copula.
@@ -15,5 +15,5 @@ Finds ``Ginv(pₓ) = p`` where ``pₓ`` is the target correlation between two ma
 function _invrule end
 
 # Uniform-Uniform
-_invrule(p::Float64, ::Uniform, ::Uniform, ::Int) = 2 * sinpi(p / 6)
-_rule(p::Float64, ::Uniform, ::Uniform, ::Int) = asin(p / 2) * 6 / π
+_invrule(p::Float64, ::Uniform, ::Uniform; kwargs...) = 2 * sinpi(p / 6)
+_rule(p::Float64, ::Uniform, ::Uniform; kwargs...) = asin(p / 2) * 6 / π

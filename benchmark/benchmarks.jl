2db1de9efa05e1c3f81edb4c447cb1fa3dbf5008
Alex Knudson <alexk.706@gmail.com>
2026-09-16 15:42:59 -0700

    Update workflows and fix benchmark workflow (#12)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,7 +7,7 @@ Logging.disable_logging(Logging.Warn)
 
 const SUITE = BenchmarkGroup()
 
-const d_all = [
+d_all = [
     Normal(10.0, 3.14),
     Binomial(25, 0.42),
     Poisson(6.8),
@@ -17,18 +17,17 @@ const d_all = [
     TDist(8),
     Beta(2.4, 5.6),
     Uniform(),
-    FDist(7, 3),
+    FDist(7, 5),
     Logistic(1.7, 2.3),
     Weibull(2.8, 5.4),
     Geometric(0.36),
     NegativeBinomial(7, 0.61),
-    Cauchy(-1.5, 0.8),
     Pareto(2.2, 0.4),
     LogNormal(-0.7, 1.1),
 ]
 
-const d_c = filter(d -> d isa ContinuousUnivariateDistribution, d_all)
-const d_d = filter(d -> d isa DiscreteUnivariateDistribution, d_all)
+d_c = filter(d -> d isa ContinuousUnivariateDistribution, d_all)
+d_d = filter(d -> d isa DiscreteUnivariateDistribution, d_all)
 
 function create_benchmark_group(distributions; seconds = 1)
     grp = BenchmarkGroup()
@@ -67,5 +66,3 @@ end
 SUITE["AllPairs"] = @benchmarkable run_all_pairs!(R, 0.32, $d_all) setup = (R = zeros(Float64, length($d_all), length($d_all)))
 SUITE["ContinuousPairs"] = @benchmarkable run_all_pairs!(R, 0.32, $d_c) setup = (R = zeros(Float64, length($d_c), length($d_c)))
 SUITE["DiscretePairs"] = @benchmarkable run_all_pairs!(R, 0.32, $d_d) setup = (R = zeros(Float64, length($d_d), length($d_d)))
-
-results = run(SUITE, verbose = true)
